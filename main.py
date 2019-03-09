52193e0ec2d70d62de2d27824f964423370918f6
Cherniakof Vlad <chernyackoff.vlad@yandex.ru>
2019-03-09 20:59:36 +0300

    createed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, url_for
 from flask import request
 
 
@@ -8,8 +8,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', source=url_for('static', filename='anchor.ico'))
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8000)
+    app.run(host="127.0.0.1", port=8000, debug=1)
