098cd0d6cedb668877a70980adb4ec4bf1703f0d
Cherniakof Vlad <chernyackoff.vlad@yandex.ru>
2019-03-10 18:09:23 +0300

    First Bug appeared

---
+++ forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SelectField, SubmitField, validators
+
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', [validators.required()])
+    email = StringField('Email', [validators.email(), validators.required()])
+    password = PasswordField('Password', [validators.required()])
+    account_type = SelectField('Account type', choices=[('usr', 'User'),
+                                                        ('dev', 'Developer')])
+    submit = SubmitField('Register')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', [validators.required()])
+    password = PasswordField('Password', [validators.required()])
+    submit = SubmitField('Login')
